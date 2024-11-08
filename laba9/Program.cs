6edd29cd96d540001494f2eb4d05a2531e526bdf
Munko Lamozhapov <lammunko@gmail.com>
2024-11-09 02:22:38 +0800

    Class Program

--- laba9/Program.cs
+++ laba9/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using laba9;
+
+class Program
+{
+    static void Main()
+    {
+        LibraryCatalog catalog = new LibraryCatalog();
+
+        // Добавление книг в каталог
+        catalog.AddBook(new Book("Война и мир", "Лев Толстой", 5, 5));
+        catalog.AddBook(new Book("Преступление и наказание", "Федор Достоевский", 3, 3));
+
+        // Печать каталога
+        catalog.PrintCatalog();
+
+        // Поиск книги по названию
+        var book = catalog.FindBookByTitle("Война и мир");
+        if (book != null)
+            Console.WriteLine($"Найденная книга: {book.ToString()}");
+
+        // Операции с книгами
+        catalog.BorrowBook("Война и мир");
+        catalog.BorrowBook("Преступление и наказание");
+
+        // Печать каталога после выдачи книги
+        catalog.PrintCatalog();
+
+        // Сохранение в XML
+        catalog.SaveToXml("library_catalog.xml");
+
+        // Загрузка из XML
+        var newCatalog = new LibraryCatalog();
+        newCatalog.LoadFromXml("library_catalog.xml");
+
+        // Печать загруженного каталога
+        newCatalog.PrintCatalog();
+    }
+}
--- laba9/obj/Debug/net8.0/laba9.AssemblyInfo.cs
+++ laba9/obj/Debug/net8.0/laba9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb1edd09cd705807ae17a0ca8b2adf2358a678e")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
